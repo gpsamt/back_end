a49fb0b1716edcc9c803a0a9c4373c79cb7f06e1
gpsamt <102432873+gpsamt@users.noreply.github.com>
2025-02-06 21:50:55 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const cityRoutes = require('./routes/cityRoutes'); // Importe as rotas das cidades
+const stateRoutes = require('./routes/stateRoutes'); // Importe as rotas de estado
 require('dotenv').config();
 
 const app = express();
@@ -9,7 +11,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', userRoutes);
+// Rotas
+app.use('/api/user', userRoutes); // Rotas de usuário
+app.use('/api/cities', cityRoutes); // Rotas de cidades
+app.use('/api/states', stateRoutes); // Rotas de estados
+
+// Rota padrão para erros 404
+app.use((req, res) => {
+  res.status(404).json({ message: 'Rota não encontrada' });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
